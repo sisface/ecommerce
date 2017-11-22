529bbd2ed10500d4e3860f9f1ea4ace3c17bf3fc
Bruce C. Miller <bm3719@gmail.com>
2017-11-22 18:22:06 -0500

    Preliminary code for first REST call

--- resources/public/script.js
+++ resources/public/script.js
@@ -6,3 +6,16 @@ $(document).ready(function() {
     var image_name = images[Math.floor(Math.random() * images.length)];
     $('#random_ad').html('<img src="img/' + image_name +'" />');
 });
+
+// Temporarily a immediately-executed function.  Change this later to be called
+// by an event handler.
+(function () {
+    $.getJSON( "/accessory", function(data) {
+        var items = [],
+            html = "";
+
+        html = 'test';
+
+        $("#content").html(items.join(html));
+    });
+})();
