317797edc057d0af2f84d4c7eeafc47d141d7e58
Angelica T. Garcia <agarcia3719@gmail.com>
2017-11-22 19:48:49 -0500

    Get item search and display working

--- resources/public/script.js
+++ resources/public/script.js
@@ -7,15 +7,33 @@ $(document).ready(function() {
     $('#random_ad').html('<img src="img/' + image_name +'" />');
 });
 
+function renderItem(item) {
+    var html = "";
+    item.forEach(function (e) {
+        html += '<img src="img/' + e.image + '" /><br />';
+        html += "name: " + e.title + "<br />";
+        html += "tag: " + e.tag + "<br />";
+        html += "description: " + e.description + "<br />";
+        html += "size: " + e.size + "<br />";
+        html += "weight: " + e.weight + "<br />";
+        html += "cost: $" + e.cost + "<br />";
+        html +=  "<br />"
+    });
+    return html;
+}
+
 // Temporarily a immediately-executed function.  Change this later to be called
 // by an event handler.
 (function () {
     $.getJSON( "/accessory", function(data) {
-        var items = [],
-            html = "";
-
-        html = 'test';
-
-        $("#content").html(items.join(html));
+        $("#content").html(renderItem(data));
     });
 })();
+
+
+$("#search_button").click(function () {
+    var selected_value = $("#category").val();
+    $.getJSON( "/" + selected_value, function(data) {
+        $("#content").html(renderItem(data));
+    });
+});
