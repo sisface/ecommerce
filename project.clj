95bf15466a0a79de9613e26473113f63d0b7e108
Angelica T. Garcia <agarcia3719@gmail.com>
2017-11-29 22:11:23 -0500

    updates comments

--- project.clj
+++ project.clj
@@ -3,11 +3,22 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]                 [cheshire "5.6.1"]
+                 ;; A routing library for REST APIs.
+                 [compojure "1.5.1"]
+                 ;; Handles JSON creation and parsing.
+                 [cheshire "5.6.1"]
+                 ;; Integration with MongoDB and a wrapper around
+                 ;; java-mongo-driver.
                  [com.novemberain/monger "3.0.1"]
+                 ;; A memoization library. Used for efficiently converting
+                 ;; Clojure variable names to Javascript ones (kebab-case vs
+                 ;; camelCase).
                  [org.clojure/core.memoize "0.5.8"]
+                 ;; Used to create embedded Jetty web server.
                  [org.eclipse.jetty/jetty-servlet "7.6.1.v20120215"]
+                 ;; Integrates Ring (what Compojure wraps) with jetty-servlet.
                  [ring/ring-jetty-adapter "1.3.2"]
+                 ;; A collection of web server settings.
                  [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler ecommerce.handler/app}
